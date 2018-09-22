710971ec95186db1203f03f43f914f5708e1d84b
Mick Phillips <mick.phillips@gmail.com>
2018-09-23 00:16:27 +0100

    Added configurable Players and trials.

--- sushiGo.py
+++ sushiGo.py
@@ -12,6 +12,7 @@ import random
 import re
 import numpy as np
 from operator import add, sub
+import weakref
 
 # Each type of card is represented by a single character. Hands
 # are represented by strings.
@@ -31,6 +32,7 @@ CHOPSTICKS = 'f'
 # A regular expression to search for paired wasabi and nigiri
 # cards in a hand.
 wasabimatch = WASABI + '[^' + SQUID + SALMON + EGG + ']*?'
+
 
 
 def sashimiscore(hand):
@@ -108,7 +110,7 @@ def puddingscores(puddings):
     return scores
 
 
-def game(nplayers):
+def newDeck():
     deck = 14 * [TEMPURA] +\
            14 * [SASHIMI] +\
            14 * [DUMPLING] +\
@@ -122,7 +124,10 @@ def game(nplayers):
             6 * [WASABI] +\
             4 * [CHOPSTICKS]
     random.shuffle(deck)
+    return deck
 
+def simpleGame(nplayers):
+    deck = newDeck()
     # Number of cards per hand each round
     ncards = {2: 10, 3: 9, 4: 8, 5: 7}[nplayers]
     nrounds = 3
@@ -144,23 +149,133 @@ def game(nplayers):
     return finalscores, roundscores
 
 
+class Player(object):
+    instances = []
+
+    def __init__(self):
+        self.__class__.instances.append(weakref.proxy(self))
+        self.hand = '';
+        self.takeFrom = self._takeFirstFrom
+        self.pref = None
+        self.puddings = 0
+
+
+    def countMaki(self):
+        c = self.hand.count
+        return 3 * c(MAKI3) + 2 * c(MAKI2) + c(MAKI1)
+
+
+    def newGame(self):
+        self.puddings = 0
+        self.hand = ''
+
+
+    def newRound(self):
+        self.puddings += self.hand.count(PUDDING)
+        self.hand = ''
+
+
+    def setPreference(self, pref):
+        if pref is None:
+            self.takeFrom = self._takeFirstFrom
+            self.pref = []
+        elif type(pref) is str:
+            if pref.lower() == 'smart':
+                self.takeFrom = self._takeSmart
+                return
+            prefs = pref.upper().split(',')
+            expansions = {'MAKI': [MAKI3, MAKI2, MAKI1],
+                          'NIGIRI': [SALMON, SQUID, EGG]}
+            self.pref = []
+            for p in prefs:
+                if not p:
+                    continue
+                if p in expansions:
+                    self.pref.extend(expansions[p])
+                else:
+                    self.pref.append(globals()[p])
+            self.takeFrom = self._takePreferenceOrFirst
+
+
+    def _takeSmart(self, hand):
+        exclude = CHOPSTICKS
+        index = 0
+
+        if len(hand) < 3 and self.hand.count(SASHIMI) % 3 < 2:
+            exclude += SASHIMI
+        if len(hand) < 3 and self.hand.count(TEMPURA) % 2 == 0:
+            exclude += TEMPURA
+        if MAKI2 in hand:
+            exclude += MAKI1
+        if MAKI3 in hand:
+            exclude += MAKI1 + MAKI2
+        if len(hand) < 3:
+            exclude += WASABI
+        if re.match(wasabimatch + '$', self.hand):
+            exclude += WASABI
+            if SQUID in hand:
+                index = hand.find(SQUID)
+        
+        if index == 0:
+            while(hand[index] in exclude and index < len(hand)-1):
+                index += 1
+        self.hand += hand[index]
+        return hand[0:index] + hand[index+1:]
+
+
+    def _takeFirstFrom(self, hand):
+        self.hand += hand[0]
+        return hand[1:]
+
+    
+    def _takePreferenceOrFirst(self, hand):
+        index = -1    
+        for p in self.pref:
+            index = hand.find(p)
+            if index != -1:
+                break
+
+        if index == -1:
+            return self._takeFirstFrom(hand)
+        else:
+            self.hand += hand[index]
+            return hand[0:index] + hand[index+1:]
+
+
+# players = [Player() for p in range(nplayers)]
+
+def trial(players):
+    from collections import deque
+    nplayers = len(players)
+    ncards = {2: 10, 3: 9, 4: 8, 5: 7}[nplayers]
+    deck = newDeck()
+    
+    [p.newGame() for p in players]
+
+    roundscores = []
+    for rnd in range(3):
+        [p.newRound() for p in players]
+        hands = np.reshape(deck[rnd * ncards * nplayers:(rnd+1) * ncards * nplayers], (nplayers, ncards))
+        hands = deque([''.join(h) for h in hands])
+        for n in range(ncards):
+            hands = deque([p.takeFrom(hands[i]) for i, p in enumerate(players)])
+            hands.rotate()
+        bscore = [basescore(p.hand) for p in players]
+        mscore = makiscores([p.hand for p in players])
+        roundscores.append(list(map(add, bscore, mscore)))
+
+    scores = [sum(r) for r in zip(*roundscores)]
+    finalscores = list(map(add, scores, puddingscores([p.puddings for p in players])))
+
+    return finalscores, roundscores
+
+
 if __name__ == '__main__':
+    import sys
 
     # Test the scoring for a random hand.
     print('=== TEST ===')
-    deck = 14 * [TEMPURA] +\
-           14 * [SASHIMI] +\
-           14 * [DUMPLING] +\
-           12 * [MAKI2] +\
-            8 * [MAKI3] +\
-            6 * [MAKI1] +\
-           10 * [SALMON] +\
-            5 * [SQUID] +\
-            5 * [EGG] +\
-           10 * [PUDDING] +\
-            6 * [WASABI] +\
-            4 * [CHOPSTICKS]
-    random.shuffle(deck)
+    deck = newDeck()
 
     hand = ''.join(deck[0:8])
     print('hand:    \t', hand)
@@ -170,24 +285,36 @@ if __name__ == '__main__':
     print('nigiri:  \t', nigiriscore(hand))
     print('=== ==== ===')
 
-    
     # Evaluate a histogram of player scores per round.
-    ngames = 100000
-    
+    ngames = 1000
+
     rounds = []
     scores = []
-    for i in range(ngames):
-        s, r = game(4)
-        rounds.append(r)
-        scores.append(s)
+
+    if len(sys.argv) == 1 or sys.argv[1].lower() == 'simple':
+        for i in range(ngames):
+            s, r = simpleGame(4)
+            rounds.append(r)
+            scores.append(s)
+    else:
+        players = []
+        for arg in sys.argv[1:]:
+            players.append(Player())
+            if arg.lower() != 'none':
+                players[-1].setPreference(arg)
+        for i in range(ngames):
+            s, r = trial(players)
+            rounds.append(r)
+            scores.append(s)
 
     rounds = np.array(rounds)
-    print ("Mean score by round across %d games:" % ngames)
-    print(rounds.mean(axis=0).mean(axis=-1))
+    print (rounds.shape)
+    print ("Mean score by player and round across %d games:" % ngames)
+    [print(row) for row in rounds.mean(axis=0)]
 
     scores = np.array(scores)
-    print ("Mean final score across %d games:" % ngames)
-    print(scores.mean())
+    print ("Mean final score by player across %d games:" % ngames)
+    print(scores.mean(axis=0))
    
     import matplotlib.pyplot as plt
     plt.hist(rounds.flatten(), bins=int( sub(rounds.max(), rounds.min()) ))
